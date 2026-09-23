191938048118a4b90f7fc116ec14218e7e7207fb
kaushal-tarun <tarunkaushalxyz1234@gmail.com>
2026-09-23 16:12:32 +0530

    chat session

--- agent.py
+++ agent.py
@@ -10,12 +10,13 @@ class Agent:
             api_key=os.getenv("GEMINI_API_KEY")
         )
 
+        self.chat_session = self.client.chats.create(
+            model="gemini-3.6-flash"
+        )
+
     def chat(self, message):
         try:
-            response = self.client.models.generate_content(
-                model="gemini-3.6-flash",
-                contents=message
-            )
+            response = self.chat_session.send_message(message)
 
             return response.text
 
